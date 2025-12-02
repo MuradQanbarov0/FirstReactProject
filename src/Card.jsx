05307e07087f5179155b265e09fbc5609d37d491
Colden111 <muradqenberov31@gmail.com>
2025-12-02 19:18:56 +0400

    commit

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card({ icon, title, description }) {
+  return (
+    <div className="card">
+      <div className="icon">
+        <img src={icon} alt={title} />
+      </div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,53 @@
+import groupIcon from "./assets/groupicons.jpeg";
+import trainer from "./assets/trainer.avif";
+import nezeriyye from "./assets/nezeriyye.png";
+import experience from "./assets/experience.png";
+import exam from "./assets/exam.png";
+import visual from "./assets/onlineandvusial.png";
+import second from "./assets/record.png";
+import first from "./assets/zemanet.png";
+
+const cardData = [
+    {
+        icon: groupIcon,
+        title: "Kiçik Qruplar",
+        description: "Dərslər kiçik qruplarda keçirilir, qruplarda 4-7 tələbə olur."
+    },
+    {
+        icon: trainer,
+        title: "Təcrübəli Təlimçilər",
+        description: "Çox peşəkar və təcrübəli təlimçilərdən dərin biliklər əldə edəcəksiniz."
+    },
+    {
+        icon: nezeriyye,
+        title: "Nəzəriyyə + Praktika",
+        description: "Hər gün tədris + praktiki məşğələ metodu ilə öyrənəcəksiniz."
+    },
+    {
+        icon: experience,
+        title: "Təcrübə proqramı və Karyera dəstəyi",
+        description: "Bitirdikdən sonra şirkətlərə yönləndirilirsiniz."
+    },
+    {
+        icon: exam,
+        title: "Ödənişsiz Sınaq Dərsləri",
+        description: "İlk dərslər tam ödənişsiz təqdim olunur."
+    },
+    {
+        icon: visual,
+        title: "Əyani və Online dərslər",
+        description: "Dərslərə əyani və ya onlayn şəkildə qoşula bilərsiniz."
+    },
+    {
+        icon: second,
+        title: "Dərslərin Video Yazıları",
+        description: "Dərslərin video yazılarını əldə edib təkrar baxa bilərsiniz."
+    },
+    {
+        icon: first,
+        title: "Zəmanət Veririk",
+        description: "Aydın olmayan mövzuları yenidən izah edirik."
+    }
+];
+
+export default cardData;
